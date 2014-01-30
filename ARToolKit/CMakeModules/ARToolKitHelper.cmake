b44fbb977f8984999cba438e0fb1bda1ba75d125
roberta4 <robertson.1992@hotmail.com>
2014-01-30 13:07:05 +0000

    FirstCommitBetter
    Committed the whole of ARToolkit along with some useful examples

---
+++ ARToolKit/CMakeModules/ARToolKitHelper.cmake
@@ -0,0 +1,234 @@
+# some simple macros to inject files into bundles
+
+set(ARTOOLKIT_FILES_DATA
+	${CMAKE_SOURCE_DIR}/bin/Data/camera_para.dat
+	${CMAKE_SOURCE_DIR}/bin/Data/object_data
+	${CMAKE_SOURCE_DIR}/bin/Data/object_data_vrml
+	${CMAKE_SOURCE_DIR}/bin/Data/object_data2
+	${CMAKE_SOURCE_DIR}/bin/Data/paddle_data
+	${CMAKE_SOURCE_DIR}/bin/Data/patt.calib
+	${CMAKE_SOURCE_DIR}/bin/Data/patt.hiro
+	${CMAKE_SOURCE_DIR}/bin/Data/patt.kanji
+	${CMAKE_SOURCE_DIR}/bin/Data/patt.sample1
+	${CMAKE_SOURCE_DIR}/bin/Data/patt.sample2
+	${CMAKE_SOURCE_DIR}/bin/Data/patt.calib
+	${CMAKE_SOURCE_DIR}/bin/Data/WDM_camera_flipV.xml
+	${CMAKE_SOURCE_DIR}/bin/Data/WDM_camera.xml
+)
+
+set(ARTOOLKIT_FILES_VRML
+	${CMAKE_SOURCE_DIR}/bin/Wrl/bud_B.dat
+	${CMAKE_SOURCE_DIR}/bin/Wrl/snoman.dat
+	${CMAKE_SOURCE_DIR}/bin/Wrl/bud_B.wrl
+	${CMAKE_SOURCE_DIR}/bin/Wrl/snoman.wrl
+)
+
+set(ARTOOLKIT_FILES_VRML_TEXTURES
+	${CMAKE_SOURCE_DIR}/bin/Wrl/textures/bud_B_Fractal_2.gif
+	${CMAKE_SOURCE_DIR}/bin/Wrl/textures/bud_B_Ramp_6.gif
+	${CMAKE_SOURCE_DIR}/bin/Wrl/textures/bud_B_Ramp_2.gif
+	${CMAKE_SOURCE_DIR}/bin/Wrl/textures/bud_B_Ramp_4.gif
+	${CMAKE_SOURCE_DIR}/bin/Wrl/textures/bud_B_Ramp_5.gif
+)
+
+set(ARTOOLKIT_FILES_DATA_WIN32
+	${CMAKE_SOURCE_DIR}/bin/Data/WDM_camera_flipV.xml
+	${CMAKE_SOURCE_DIR}/bin/Data/WDM_camera.xml
+)
+
+set(ARTOOLKIT_FILES_DATA_MULTI
+	${CMAKE_SOURCE_DIR}/bin/Data/multi/marker.dat
+	${CMAKE_SOURCE_DIR}/bin/Data/multi/patt.a
+	${CMAKE_SOURCE_DIR}/bin/Data/multi/patt.b
+	${CMAKE_SOURCE_DIR}/bin/Data/multi/patt.c
+	${CMAKE_SOURCE_DIR}/bin/Data/multi/patt.d
+	${CMAKE_SOURCE_DIR}/bin/Data/multi/patt.f
+	${CMAKE_SOURCE_DIR}/bin/Data/multi/patt.g
+)
+
+
+macro(artoolkit_executable EXE_NAME SRCS)
+	
+	if(APPLE)
+	
+		set(MACOSX_BUNDLE_BUNDLE_NAME           ${EXE_NAME})
+		set(MACOSX_BUNDLE_BUNDLE_VERSION        ${ARTOOLKIT_VERSION_SHORT})
+		set(MACOSX_BUNDLE_SHORT_VERSION_STRING  ${ARTOOLKIT_VERSION_FULL})
+		set(MACOSX_BUNDLE_LONG_VERSION_STRING   "ARToolKit ${EXE_NAME} Version ${ARTOOLKIT_VERSION_FULL}")
+		set(MACOSX_BUNDLE_ICON_FILE             ARToolKit.icns)	
+		set(MACOSX_BUNDLE_COPYRIGHT             "(c) 2008 Human Interface Technology Laboratory New Zealand")
+		
+		set_source_files_properties(
+			${CMAKE_SOURCE_DIR}/share/ARToolKit.icns
+			PROPERTIES
+			MACOSX_PACKAGE_LOCATION "Resources"
+			)
+		set_source_files_properties(
+			${ARTOOLKIT_FILES_DATA}
+			PROPERTIES
+			HEADER_FILE_ONLY TRUE
+			MACOSX_PACKAGE_LOCATION "Resources/Data"
+			)
+			
+		set_source_files_properties(
+			${ARTOOLKIT_FILES_DATA_MULTI}
+			PROPERTIES
+			HEADER_FILE_ONLY TRUE
+			MACOSX_PACKAGE_LOCATION "Resources/Data/multi"
+			)
+			
+		add_executable(${EXE_NAME} MACOSX_BUNDLE 
+			${${SRCS}} ${CMAKE_SOURCE_DIR}/share/ARToolKit.icns
+			${ARTOOLKIT_FILES_DATA}
+			${ARTOOLKIT_FILES_DATA_MULTI}
+			)
+			
+	else(APPLE)
+		set(TEMP_RC_NAME ${CMAKE_BINARY_DIR}/.rc/${EXE_NAME}_temp.rc)
+		
+		configure_file(
+			${CMAKE_SOURCE_DIR}/share/ARToolKit.rc.in
+			${TEMP_RC_NAME}
+			IMMEDIATE @ONLY
+		)
+	
+		add_executable(${EXE_NAME} ${${SRCS}} ${TEMP_RC_NAME})
+	endif(APPLE)
+endmacro(artoolkit_executable)
+
+
+macro(artoolkit_install target)
+
+	if   (WIN32)
+		set(lib_dest bin)
+	else (WIN32)
+		set(lib_dest lib)
+	endif(WIN32)
+	
+	set(cmake_version_short "${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}")
+
+	if (cmake_version_short GREATER "2.5")
+		install(TARGETS ${target}
+			BUNDLE DESTINATION "/Applications/ARToolKit-${ARTOOLKIT_VERSION_FULL}"
+			RUNTIME DESTINATION bin
+			LIBRARY DESTINATION ${lib_dest}	
+			ARCHIVE DESTINATION lib
+			PUBLIC_HEADER DESTINATION include/AR
+			PERMISSIONS 
+			OWNER_EXECUTE OWNER_WRITE OWNER_READ
+			GROUP_EXECUTE GROUP_READ
+			WORLD_EXECUTE WORLD_READ
+			)
+	else(cmake_version_short GREATER "2.5")
+		install(TARGETS ${target}
+			RUNTIME DESTINATION bin
+			LIBRARY DESTINATION ${lib_dest}	
+			ARCHIVE DESTINATION lib
+			PERMISSIONS 
+			OWNER_EXECUTE OWNER_WRITE OWNER_READ
+			GROUP_EXECUTE GROUP_READ
+			WORLD_EXECUTE WORLD_READ
+			)
+	endif(cmake_version_short GREATER "2.5")
+		
+endmacro(artoolkit_install target)
+
+
+
+macro(artoolkit_lib_install target)
+
+	artoolkit_install(target)
+		
+endmacro(artoolkit_lib_install target)
+
+
+
+
+macro(artoolkit_example_lite name source_files)
+
+	set(exe_name ${name})
+	
+	include_directories(${OPENGL_INCLUDE_DIR} ${GLUT_INCLUDE_DIR})
+
+	artoolkit_executable(${exe_name} ${source_files})
+
+	target_link_libraries(${exe_name} 
+		AR 
+		ARvideo
+		ARMulti
+		ARgsubUtil
+		ARgsub_lite
+		${OPENGL_LIBRARIES} ${GLUT_LIBRARIES}
+		)
+		
+	set_target_properties(${exe_name}
+		PROPERTIES
+		PROJECT_LABEL "Example ${name}"
+	)
+
+	artoolkit_install(${exe_name})	
+
+endmacro(artoolkit_example_lite name source_files)
+
+
+macro(artoolkit_example_glut name source_files)
+
+	set(exe_name ${name})
+	
+	include_directories(${OPENGL_INCLUDE_DIR} ${GLUT_INCLUDE_DIR})
+	
+	artoolkit_executable(${exe_name} ${source_files})
+
+	target_link_libraries(${exe_name} 
+		AR 		
+		ARvideo
+		ARgsub
+		ARgsubUtil		
+		ARMulti
+		${OPENGL_LIBRARIES} ${GLUT_LIBRARIES}
+		)
+		
+	set_target_properties(${exe_name}
+		PROPERTIES
+		PROJECT_LABEL "Example ${name}"
+	)
+
+	artoolkit_install(${exe_name})	
+
+endmacro(artoolkit_example_glut name source_files)
+
+
+macro(artoolkit_utility_glut name source_files)
+
+	set(exe_name ${name})
+	
+	include_directories(${OPENGL_INCLUDE_DIR} ${GLUT_INCLUDE_DIR})
+	
+	artoolkit_executable(${exe_name} ${source_files})
+
+	target_link_libraries(${exe_name} 
+		AR 
+		ARvideo
+		ARMulti		
+		ARgsub
+		ARgsub_lite
+		ARgsubUtil
+		${OPENGL_LIBRARIES} ${GLUT_LIBRARIES}
+		)
+		
+	set_target_properties(${exe_name}
+		PROPERTIES
+		PROJECT_LABEL "Utility ${name}"
+	)
+
+	artoolkit_install(${exe_name})	
+
+endmacro(artoolkit_utility_glut name source_files)
+
+
+macro(artoolkit_framework)
+	
+	
+
+endmacro(artoolkit_framework)
+
---
+++ ARToolKit/CMakeModules/FindOpenVRML.cmake
@@ -0,0 +1,188 @@
+# Locate openvml library
+# This module defines
+# OPENVRML_LIBRARY
+# OPENVRML_FOUND, if false, do not try to link to vrml 
+# OPENVRML_INCLUDE_DIR, where to find the headers
+#
+# $OPENVRML_DIR is an environment variable that would
+# correspond to the ./configure --prefix=$OPENVRML_DIR
+#
+# Created by Robert Osfield. 
+# Modified for the debug library by Jean-S?stien Guay.
+# Modified by Hartmut Seichter for inclusion into ARToolKit
+#
+
+
+set(OPENVRML_ROOT_DIR CACHE PATH "OpenVRML Binary root directory")
+	
+	
+FIND_PATH(OPENVRML_INCLUDE_DIR openvrml/system.h
+	${CMAKE_SOURCE_DIR}/../openvrml-0.16.6-bin-macosx/include
+    $ENV{OPENVRML_DIR}/include
+    $ENV{OPENVRML_DIR}
+    $ENV{OSGDIR}/include
+    $ENV{OSGDIR}
+    $ENV{OSG_ROOT}/include
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/include
+    /usr/include
+    /sw/include # Fink
+    /opt/local/include # DarwinPorts
+    /opt/csw/include # Blastwave
+    /opt/include
+    [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OSG_ROOT]/include
+    /usr/freeware/include
+)
+
+
+if(OPENVRML_INCLUDE_DIR)
+	set(OPENVRML_ROOT_DIR ${OPENVRML_INCLUDE_DIR}/../)
+endif(OPENVRML_INCLUDE_DIR)
+
+set(OPENVRML_LIB_SEARCH_PATHS
+	${CMAKE_SOURCE_DIR}/../openvrml-0.16.6-bin-macosx/lib
+    $ENV{OPENVRML_DIR}/lib
+    $ENV{OPENVRML_DIR}
+    $ENV{OSGDIR}/lib
+    $ENV{OSGDIR}
+    $ENV{OSG_ROOT}/lib
+	${OPENVRML_ROOT_DIR}/lib
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/lib
+    /usr/lib
+    /sw/lib
+    /opt/local/lib
+    /opt/csw/lib
+    /opt/lib
+    [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OSG_ROOT]/lib
+    /usr/freeware/lib64
+	)
+
+
+FIND_LIBRARY(OPENVRML_gl_LIBRARY 
+	NAMES openvrml-gl
+	PATHS
+	${OPENVRML_LIB_SEARCH_PATHS}
+)
+
+FIND_LIBRARY(OPENVRML_openvrml_LIBRARY 
+	NAMES openvrml
+	PATHS
+	${OPENVRML_LIB_SEARCH_PATHS}
+)
+
+IF(APPLE)
+	FIND_LIBRARY(OPENVRML_jpeg_LIBRARY 
+		NAMES jpeg
+		PATHS
+		${OPENVRML_LIB_SEARCH_PATHS}
+	)
+
+	FIND_LIBRARY(OPENVRML_png_LIBRARY 
+		NAMES png
+		PATHS
+		${OPENVRML_LIB_SEARCH_PATHS}
+	)
+
+
+	FIND_LIBRARY(OPENVRML_js_LIBRARY 
+		NAMES js
+		PATHS
+		${OPENVRML_LIB_SEARCH_PATHS}
+	)
+
+	FIND_LIBRARY(OPENVRML_boost_LIBRARY 
+		NAMES boost_thread-1_33_1
+		PATHS
+		${OPENVRML_LIB_SEARCH_PATHS}
+	)
+
+	FIND_LIBRARY(OPENVRML_plds4_LIBRARY 
+		NAMES plds4
+		PATHS
+		${OPENVRML_LIB_SEARCH_PATHS}
+	)
+
+	FIND_LIBRARY(OPENVRML_nspr4_LIBRARY 
+		NAMES nspr4
+		PATHS
+		${OPENVRML_LIB_SEARCH_PATHS}
+	)
+
+	FIND_LIBRARY(OPENVRML_plc4_LIBRARY 
+		NAMES plc4
+		PATHS
+		${OPENVRML_LIB_SEARCH_PATHS}
+	)
+ENDIF(APPLE)
+
+IF(WIN32)
+	FIND_LIBRARY(OPENVRML_boost_LIBRARY 
+		NAMES 
+		boost_thread-1_34_1
+		libboost_thread-vc80-mt-1_34_1
+		PATHS
+		${OPENVRML_LIB_SEARCH_PATHS}
+	)
+ENDIF(WIN32)
+
+FIND_LIBRARY(OPENVRML_LIBRARY_DEBUG 
+    NAMES openvrmld
+    PATHS
+    $ENV{OPENVRML_DIR}/lib
+    $ENV{OPENVRML_DIR}
+    $ENV{OSGDIR}/lib
+    $ENV{OSGDIR}
+    $ENV{OSG_ROOT}/lib
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/lib
+    /usr/lib
+    /sw/lib
+    /opt/local/lib
+    /opt/csw/lib
+    /opt/lib
+    [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OSG_ROOT]/lib
+    /usr/freeware/lib64
+)
+
+SET(OPENVRML_FOUND "NO")
+IF(OPENVRML_openvrml_LIBRARY AND OPENVRML_INCLUDE_DIR)
+    SET(OPENVRML_FOUND "YES")
+	SET(OPENVRML_LIBRARIES
+		${OPENVRML_openvrml_LIBRARY}
+		${OPENVRML_gl_LIBRARY}
+		${OPENVRML_boost_LIBRARY}
+		)
+	if(APPLE)
+		list(APPEND
+			${OPENVRML_jpeg_LIBRARY}
+			${OPENVRML_js_LIBRARY}
+			${OPENVRML_png_LIBRARY}
+			${OPENVRML_plc4_LIBRARY}
+			${OPENVRML_nspr4_LIBRARY}
+			${OPENVRML_plds4_LIBRARY}
+		)
+	endif(APPLE)
+ENDIF(OPENVRML_openvrml_LIBRARY AND OPENVRML_INCLUDE_DIR)
+
+
+
+#-------------- next part --------------
+IF (WIN32)
+    INCLUDE_DIRECTORIES( ${OPENVRML_INCLUDE_DIR} ${OPENVRML_INCLUDE_DIR}/openvrml ${JPEG_INCLUDE_DIR} ${PNG_INCLUDE_DIR} ${ZLIB_INCLUDE_DIR})
+	
+#   SET(OPENVRML_ANTLR_LIBRARY       antlr.lib)
+#   SET(OPENVRML_ANTLR_LIBRARY_DEBUG antlrd.lib)
+#   SET(OPENVRML_REGEX_LIBRARY       regex.lib)
+#   SET(OPENVRML_REGEX_LIBRARY_DEBUG regexd.lib)
+#	SET(TARGET_LIBRARIES_VARS OPENVRML_ANTLR_LIBRARY OPENVRML_REGEX_LIBRARY OPENVRML_LIBRARY JPEG_LIBRARY PNG_LIBRARY ZLIB_LIBRARY)
+    SET(TARGET_EXTERNAL_LIBRARIES Ws2_32.lib)
+	
+ELSE(WIN32)
+    INCLUDE_DIRECTORIES( ${OPENVRML_INCLUDE_DIR} ${OPENVRML_INCLUDE_DIR}/openvrml)
+ENDIF(WIN32)
+
+
